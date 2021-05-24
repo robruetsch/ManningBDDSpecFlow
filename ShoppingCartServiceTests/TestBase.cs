8bd84ccf50d077f717cb6d7eb15d4ca2ed90d7e1
Rob Ruetsch <rob.ruetsch@gmail.com>
2021-05-24 18:36:42 +0200

    Add project files.

---
+++ ShoppingCartServiceTests/TestBase.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+using AutoMapper;
+using ShoppingCartService.Mapping;
+
+// Add assembly level trait for all tests in this assembly
+[assembly: AssemblyTrait("Build", "CI")]
+
+namespace ShoppingCartServiceTests
+{
+    
+    // Put common infrastructure in here
+    // Never, ever put test logic or initialize tested classes in this class
+    public abstract class TestBase
+    {
+        protected TestBase()
+        {
+            // Ideally do not write any test related logic here
+            // Only infrastructure and environment setup
+
+            var config = new MapperConfiguration(cfg => cfg.AddProfile(new MappingProfile()));
+
+            Mapper = config.CreateMapper();
+        }
+
+        protected IMapper Mapper { get; }
+    }
+}
